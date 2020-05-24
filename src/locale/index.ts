0fd8c740c996e49426e811967a98dcda7d595621
dimitri <dimitri@staritgp.com>
2020-05-25 01:37:08 +0800

    UPDATE 重置注释

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/camelcase */
 import Jed from 'jed'
 import fs from 'fs'
 import path from 'path'
@@ -6,16 +8,10 @@ import { parse } from 'po2json'
 import { app } from 'electron'
 console.log(po)
 console.log(app.getPath('logs'))
-// eslint-disable-next-line camelcase
-// eslint-disable-next-line @typescript-eslint/camelcase
 const en_US = parse(
   fs.readFileSync(path.resolve(app.getAppPath(), './build/locale/en_US.po')),
   { format: 'jed' }
 )
-// eslint-disable-next-line camelcase
-// eslint-disable-next-line no-unused-vars
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// eslint-disable-next-line @typescript-eslint/camelcase
 const zh_CN = parse(
   fs.readFileSync(path.resolve(app.getAppPath(), './build/locale/zh_CN.po')),
   { format: 'jed' }
